7edbdc3e2925df711475dc2e620dd355b93375e2
Saul Ibarra <saul@ag-projects.com>
2011-03-21 09:21:18 +0000

    Raised python-sipsimple version dependency

--- sylk/__init__.py
+++ sylk/__init__.py
@@ -8,7 +8,7 @@ configuration_filename = "config.ini"
 
 
 package_requirements = {'python-application': '1.2.6',
-                        'python-sipsimple': '0.17.1'}
+                        'python-sipsimple': '0.18.0'}
 
 try:
     from application.dependency import ApplicationDependencies
