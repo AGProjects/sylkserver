7a536bc1de844173bc620df8a5c932ab5394281e
Adrian Georgescu <ag@ag-projects.com>
2013-01-23 13:45:06 +0000

    Use lower case

--- sylk/applications/xmppgateway/__init__.py
+++ sylk/applications/xmppgateway/__init__.py
@@ -144,14 +144,14 @@ class XMPPGatewayApplication(object):
         log.msg('SIP subscription from %s to %s' % (format_uri(from_header.uri, 'sip'), format_uri(to_header.uri, 'xmpp')))
 
         if subscribe_request.event != 'presence':
-            log.msg('SIP Subscription rejected: only presence event is supported')
+            log.msg('SIP subscription rejected: only presence event is supported')
             subscribe_request.reject(489)
             return
 
         # Check domain
         remote_identity_uri = data.headers['From'].uri
         if remote_identity_uri.host not in XMPPGatewayConfig.domains:
-            log.msg('SIP Subscription rejected: From domain is not a local XMPP domain')
+            log.msg('SIP subscription rejected: From domain is not a local XMPP domain')
             subscribe_request.reject(606)
             return
 
