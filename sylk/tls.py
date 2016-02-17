8e617e9cd8e1ef5bceba519397bca39a145c7f2f
Saul Ibarra <saul@ag-projects.com>
2016-02-17 11:16:41 +0000

    Style

--- sylk/tls.py
+++ sylk/tls.py
@@ -25,7 +25,7 @@ def file_content(file):
 class Certificate(object):
     """Configuration data type. Used to create a gnutls.crypto.X509Certificate object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, basestring):
             try:
                 return X509Certificate(file_content(value))
@@ -33,12 +33,12 @@ class Certificate(object):
                 log.warn("Certificate file '%s' could not be loaded: %s" % (value, str(e)))
                 return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
 
 class PrivateKey(object):
     """Configuration data type. Used to create a gnutls.crypto.X509PrivateKey object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, basestring):
             try:
                 return X509PrivateKey(file_content(value))
@@ -46,5 +46,5 @@ class PrivateKey(object):
                 log.warn("Private key file '%s' could not be loaded: %s" % (value, str(e)))
                 return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
 
