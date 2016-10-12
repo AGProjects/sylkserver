04f82cb2c94c167bbf4542bd98ef1640d82f55e9
Saul Ibarra <saul@ag-projects.com>
2016-10-12 05:40:26 +0000

    Don't use self-signed certificate for the web server by default
    Connections to the WebSocket endpoint will silently fail and it makes it
    hard to debug.

--- sylk/configuration/__init__.py
+++ sylk/configuration/__init__.py
@@ -69,7 +69,7 @@ class WebServerConfig(ConfigSection):
     local_ip = ConfigSetting(type=IPAddress, value=IPAddress(host.default_ip))
     local_port = ConfigSetting(type=Port, value=10888)
     hostname = ''
-    certificate = ConfigSetting(type=Path, value=Path(Resources.get('tls/default.crt')))
+    certificate = ConfigSetting(type=Path, value=None)
     certificate_chain = ConfigSetting(type=Path, value=None)
 
 
