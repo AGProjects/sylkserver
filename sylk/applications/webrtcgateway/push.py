ce0ae28cf7733adffdf128f7f50efe30634c707d
Tijmen de Mes <tijmen@ag-projects.com>
2020-07-10 12:53:41 +0000

    Fixed syntax error

--- sylk/applications/webrtcgateway/push.py
+++ sylk/applications/webrtcgateway/push.py
@@ -67,7 +67,7 @@ def _send_push_notification(payload, destination):
         except IndexError:
             pass
         try:
-            r = yield agent.request('POST', GeneralConfig.sylk_push_url, headers, StringProducer(payload.__data__)
+            r = yield agent.request('POST', GeneralConfig.sylk_push_url, headers, StringProducer(payload.__data__))
         except Exception as e:
             log.info('Error sending push notification to %s: %s', GeneralConfig.sylk_push_url, e)
         else:
