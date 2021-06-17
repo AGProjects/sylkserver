5b7e1da5d4299c12254ebe778f80888f69e1c4e1
Adrian Georgescu <ag@ag-projects.com>
2021-06-17 12:34:27 +0000

    Discard observer

--- sylk/session.py
+++ sylk/session.py
@@ -1881,7 +1881,7 @@ class Session(object):
                     else:
                         notification.center.post_notification('SIPSessionWillEnd', self, NotificationData(originator=notification.data.originator))
                         for stream in self.streams:
-                            notification.center.remove_observer(self, sender=stream)
+                            notification.center.discard_observer(self, sender=stream)
                             stream.deactivate()
                             stream.end()
                         self.state = 'terminated'
