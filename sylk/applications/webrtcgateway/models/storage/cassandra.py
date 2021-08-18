7005e27c07249f34b94d568ae83d8e9e17542b42
Tijmen de Mes <tijmen@ag-projects.com>
2021-08-18 11:05:09 +0000

    Added cassandra models for message storage

--- sylk/applications/webrtcgateway/models/storage/cassandra.py
+++ sylk/applications/webrtcgateway/models/storage/cassandra.py
@@ -14,3 +14,40 @@ class PushTokens(Model):
     platform         = columns.Text()
     silent           = columns.Text()
     user_agent       = columns.Text(required=False)
+
+
+class ChatMessage(Model):
+    __table_name__  = 'chat_messages_by_timestamp'
+    __options__     = {'default_time_to_live': '31536000',
+                        'gc_grace_seconds': '345600'}
+    account         = columns.Text(partition_key=True)
+    contact         = columns.Text()
+    created_at      = columns.DateTime(primary_key=True)
+    direction       = columns.Text()
+    message_id      = columns.Text(primary_key=True)
+    content         = columns.Text()
+    content_type    = columns.Text()
+    disposition     = columns.List(value_type=columns.Text, required=False)
+    state           = columns.Text()
+    msg_timestamp   = columns.DateTime()
+
+
+class ChatMessageIdMapping(Model):
+    __table_name__  = 'chat_message_created_at_by_id'
+    __options__     = {'default_time_to_live': '31536000',
+                        'gc_grace_seconds': '345600'}
+    message_id      = columns.Text(partition_key=True)
+    created_at      = columns.DateTime()
+
+
+class PublicKey(Model):
+    __table_name__   = 'public_key_by_account'
+    public_key       = columns.Text()
+    account          = columns.Text(partition_key=True)
+
+
+class ChatAccount(Model):
+    __table_name__   = 'chat_accounts'
+    account          = columns.Text(partition_key=True)
+    api_token        = columns.Text()
+    last_login       = columns.DateTime()
